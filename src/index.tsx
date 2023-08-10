97d53bb38e774558297259d9f7a159560d5a0c72
karolcossatis <karolcossatis@gmail.com>
2023-08-09 21:19:07 -0300

    first

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'normalize.css';
+import './index.css';
+import Cardapio from './pages/cardapio/index';
+import Inicio from 'pages/Inicio';
+import Router from './routes';
+
+
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Router/>
+  </React.StrictMode>
+);
+
+
+
---
+++ src/pages/cardapio/Itens/Item/index.tsx
@@ -0,0 +1,26 @@
+import styles from './Item.module.scss'; 
+import TagsPrato from 'components/TagsPrato';
+import { Prato } from 'types/Prato';
+
+
+
+
+export default function Item(props: Prato ) {
+  const { title, description,  photo } = props; 
+
+  return (
+    <div className={styles.item}>
+      <div className={styles.item__imagem}>
+        <img src={photo} alt={title} />
+      </div>
+      <div className={styles.item__descricao}>
+        <div className={styles.item__titulo}>
+      
+          <h2> {title} </h2>
+          <p>{description} </p>
+        </div>
+        <TagsPrato {...props}/>
+      </div>
+    </div>
+  );
+}
